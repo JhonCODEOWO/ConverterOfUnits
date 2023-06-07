0cea1748bc7feab1cc1fb29e10f6f58147a28eb0
Jhon <jjv20618@gmail.com>
2023-06-06 19:10:03 -0600

    First submit

---
+++ Convertidor_De_Peso/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Convertidor_De_Peso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMedidaI = new System.Windows.Forms.TextBox();
+            this.txtTonelada = new System.Windows.Forms.TextBox();
+            this.txtKiloGramo = new System.Windows.Forms.TextBox();
+            this.txtGramo = new System.Windows.Forms.TextBox();
+            this.txtMilGramo = new System.Windows.Forms.TextBox();
+            this.txtMicGramo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbMedidas = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escriba la cantidad para mostrar\r\nen los distintos campos:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tonelada:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gramo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Miligramo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Microgramo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kilogramo:";
+            // 
+            // txtMedidaI
+            // 
+            this.txtMedidaI.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMedidaI.Location = new System.Drawing.Point(15, 114);
+            this.txtMedidaI.Name = "txtMedidaI";
+            this.txtMedidaI.Size = new System.Drawing.Size(100, 23);
+            this.txtMedidaI.TabIndex = 6;
+            // 
+            // txtTonelada
+            // 
+            this.txtTonelada.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTonelada.Location = new System.Drawing.Point(126, 350);
+            this.txtTonelada.Name = "txtTonelada";
+            this.txtTonelada.ReadOnly = true;
+            this.txtTonelada.Size = new System.Drawing.Size(167, 23);
+            this.txtTonelada.TabIndex = 7;
+            // 
+            // txtKiloGramo
+            // 
+            this.txtKiloGramo.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKiloGramo.Location = new System.Drawing.Point(126, 309);
+            this.txtKiloGramo.Name = "txtKiloGramo";
+            this.txtKiloGramo.ReadOnly = true;
+            this.txtKiloGramo.Size = new System.Drawing.Size(167, 23);
+            this.txtKiloGramo.TabIndex = 8;
+            // 
+            // txtGramo
+            // 
+            this.txtGramo.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGramo.Location = new System.Drawing.Point(126, 270);
+            this.txtGramo.Name = "txtGramo";
+            this.txtGramo.ReadOnly = true;
+            this.txtGramo.Size = new System.Drawing.Size(167, 23);
+            this.txtGramo.TabIndex = 9;
+            // 
+            // txtMilGramo
+            // 
+            this.txtMilGramo.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMilGramo.Location = new System.Drawing.Point(126, 232);
+            this.txtMilGramo.Name = "txtMilGramo";
+            this.txtMilGramo.ReadOnly = true;
+            this.txtMilGramo.Size = new System.Drawing.Size(167, 23);
+            this.txtMilGramo.TabIndex = 10;
+            // 
+            // txtMicGramo
+            // 
+            this.txtMicGramo.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMicGramo.Location = new System.Drawing.Point(126, 194);
+            this.txtMicGramo.Name = "txtMicGramo";
+            this.txtMicGramo.ReadOnly = true;
+            this.txtMicGramo.Size = new System.Drawing.Size(167, 23);
+            this.txtMicGramo.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbMedidas
+            // 
+            this.cmbMedidas.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMedidas.FormattingEnabled = true;
+            this.cmbMedidas.Items.AddRange(new object[] {
+            "ug",
+            "Mg",
+            "Gr",
+            "Kg",
+            "T"});
+            this.cmbMedidas.Location = new System.Drawing.Point(121, 114);
+            this.cmbMedidas.Name = "cmbMedidas";
+            this.cmbMedidas.Size = new System.Drawing.Size(51, 24);
+            this.cmbMedidas.TabIndex = 13;
+            this.cmbMedidas.Text = "--";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label7.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(0, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(305, 26);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Convertidor de peso\r\n";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel3,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(305, 25);
+            this.toolStrip1.TabIndex = 15;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(59, 22);
+            this.toolStripLabel1.Text = "Acerca de";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(10, 22);
+            this.toolStripLabel3.Text = "|";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(77, 22);
+            this.toolStripLabel2.Text = "Instrucciones";
+            this.toolStripLabel2.Click += new System.EventHandler(this.toolStripLabel2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(305, 387);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbMedidas);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMicGramo);
+            this.Controls.Add(this.txtMilGramo);
+            this.Controls.Add(this.txtGramo);
+            this.Controls.Add(this.txtKiloGramo);
+            this.Controls.Add(this.txtTonelada);
+            this.Controls.Add(this.txtMedidaI);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Inicio";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMedidaI;
+        private System.Windows.Forms.TextBox txtTonelada;
+        private System.Windows.Forms.TextBox txtKiloGramo;
+        private System.Windows.Forms.TextBox txtGramo;
+        private System.Windows.Forms.TextBox txtMilGramo;
+        private System.Windows.Forms.TextBox txtMicGramo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbMedidas;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+    }
+}
+
---
+++ Convertidor_De_Peso/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convertidor_De_Peso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double miligramo;
+        double microgramo;
+        double gramo;
+        double kilogramo;
+        double tonelada;
+        double MIni; //variable para almcenar la medida inicial
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtMedidaI.Text.Length == 0)
+            {
+                MessageBox.Show("Porfavor escribe una cantidad\ndentro del cuadro de texto\nY selecciona una unidad de peso", "¡Advertencia!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MIni = Convert.ToDouble(txtMedidaI.Text);
+                calcular_microgramos();
+                convertir_a_miligramo();
+                convertir_a_gramos();
+                convertir_a_kg();
+                convertir_a_toneladas();
+                txtMicGramo.Text = Convert.ToString(microgramo);
+                txtMilGramo.Text = Convert.ToString(miligramo);
+                txtGramo.Text = Convert.ToString(gramo);
+                txtKiloGramo.Text = Convert.ToString(kilogramo);
+                txtTonelada.Text = Convert.ToString(tonelada);
+            }
+        }
+        private void calcular_microgramos()
+        {
+            if(cmbMedidas.SelectedIndex==0)
+            {
+                microgramo = MIni;
+            }
+            if(cmbMedidas.SelectedIndex==1)
+            {
+                microgramo = MIni / 0.0010000; //ecuacion para miligramos
+            }
+            if(cmbMedidas.SelectedIndex==2)
+            {
+                microgramo = MIni / 0.0000010000; //ecuacion de gr
+            }
+            if(cmbMedidas.SelectedIndex==3)
+            {
+                microgramo = MIni / 0.0000000010000; //ecuacion para kg
+            }
+            if (cmbMedidas.SelectedIndex == 4)
+            {
+                microgramo = MIni / 0.0000000000010000; //ecuacion en t
+            }
+        }
+        private void convertir_a_miligramo()
+        {
+            if(cmbMedidas.SelectedIndex==0)
+            {
+                miligramo = MIni / 1000.0; //mic a miligramo
+            }
+            if(cmbMedidas.SelectedIndex==1)
+            {
+                miligramo = MIni;
+            }
+            if(cmbMedidas.SelectedIndex==2)
+            {
+                miligramo = MIni / 0.0010000; //gr a mg
+            }
+            if(cmbMedidas.SelectedIndex==3)
+            {
+                miligramo = MIni / 0.0000010000; // kg a 
+            }
+            if (cmbMedidas.SelectedIndex == 4)
+            {
+                miligramo = MIni / 0.0000000010000;
+            }
+        }
+        private void convertir_a_gramos()
+        {
+            if(cmbMedidas.SelectedIndex==0)
+            {
+                gramo = MIni / 1000000;// microg a gr
+            }
+            if(cmbMedidas.SelectedIndex==1)
+            {
+                gramo = MIni / 1000.0; // mg a gr
+            }
+            if(cmbMedidas.SelectedIndex==2)
+            {
+                gramo = MIni; // gr
+            }
+            if(cmbMedidas.SelectedIndex==3)
+            {
+                gramo = MIni / 0.0010000; //kg a gr
+            }
+            if (cmbMedidas.SelectedIndex == 4)
+            {
+                gramo = MIni / 0.0000010000; //t a gr
+            }
+        }
+        private void convertir_a_kg()
+        {
+            if(cmbMedidas.SelectedIndex==0)
+            {
+                kilogramo = MIni / 1000000000; //micg a kg
+            }
+            if(cmbMedidas.SelectedIndex==1)
+            {
+                kilogramo = MIni / 1000000; // mg a kg
+            }
+            if(cmbMedidas.SelectedIndex==2)
+            {
+                kilogramo= MIni / 1000.0 ; //gr a kg
+            }
+            if(cmbMedidas.SelectedIndex==3)
+            {
+                kilogramo = MIni; //kg
+            }
+            if (cmbMedidas.SelectedIndex == 4)
+            {
+                kilogramo = MIni / 0.0010000; //t a kg
+            }
+        }
+        private void convertir_a_toneladas()
+        {
+            if(cmbMedidas.SelectedIndex==0)
+            {
+                tonelada = MIni / 1000000000000; //micg a t
+            }
+            if(cmbMedidas.SelectedIndex==1)
+            {
+                tonelada = MIni / 1000000000; //mg a t 
+            }
+            if(cmbMedidas.SelectedIndex==2)
+            {
+                tonelada = MIni / 1000000; //gr a t
+            }
+            if(cmbMedidas.SelectedIndex==3)
+            {
+                tonelada = MIni / 1000.0; //kg a t
+            }
+            if (cmbMedidas.SelectedIndex == 4)
+            {
+                tonelada = MIni; // t 
+            }
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Designed by: Jonathan J.V\nVersion: 1.0", "Acerca De", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void toolStripLabel2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Escribe en el cuadro de texto la cantidad y seleccione \nla unidad de medida del lado derecho", "Instrucciones", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
